99d643fb29fa663fe0ecd7c8d0097c55d104c6a0
trumanharp-tamu <trumanharp@tamu.edu>
2025-07-20 18:57:53 -0500

    load balancer header

---
+++ LoadBalancer.h
@@ -0,0 +1,19 @@
+#include "WebServer.h"
+#include "RequestQueue.h"
+#include <vector>
+
+using std::vector;
+
+class LoadBalancer {
+    int numServers;
+    int totalCycles;
+    int cyclesLeft;
+    int percentChanceGenerateRequest;
+
+    vector<WebServer> servers;
+    RequestQueue requestQueue;
+
+public:
+    LoadBalancer(int servers, int totalCycles, int percentChanceGenerateRequest);
+    void runCycle();
+};
