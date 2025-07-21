848e0d80a74052b978f0f131deeb5dc7005b3aa1
trumanharp-tamu <trumanharp@tamu.edu>
2025-07-20 19:32:50 -0500

    load balancer cpp start

---
+++ LoadBalancer.cpp
@@ -0,0 +1,21 @@
+#include "LoadBalancer.h"
+
+LoadBalancer::LoadBalancer(int numServers, int totalCycles, int percentChanceGenerateRequest) 
+    : numServers(numServers), totalCycles(totalCycles), percentChanceGenerateRequest(percentChanceGenerateRequest), cyclesLeft(totalCycles), servers(numServers) {
+
+    requestQueue.fillQueue(numServers * 100);
+    //TODO Log starter queue size
+}
+
+void  LoadBalancer::run() {
+    while (cyclesLeft > 0) {
+        runCycle();
+        cyclesLeft--;
+    }
+    //TODO Log final state of servers and request queue
+}
+
+void LoadBalancer::runCycle() {
+    //todo main logic for a cycle
+}
+
